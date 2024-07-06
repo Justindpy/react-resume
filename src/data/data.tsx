75b53f03acb060df5cb7e71fe7f3c959ac93547f
Justindpy <justin.alderath@gmail.com>
2024-07-06 16:10:39 +0200

    another change

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -12,9 +12,9 @@ import GithubIcon from '../components/Icon/GithubIcon';
 import InstagramIcon from '../components/Icon/InstagramIcon';
 import LinkedInIcon from '../components/Icon/LinkedInIcon';
 import heroImage from '../images/header-background.webp';
+import mobilefusspflegelogo from '../images/portfolio/justin_logo_lilievorne-removebg-preview.png';
 //import getjustitlogo from '../images/portfolio/gejustitlogo.jpeg';
 import stahltonlogo from '../images/portfolio/Logo_StahlTon_Grau-1.png';
-import mobilefusspflegelogo from '../images/portfolio/justin_logo_lilievorne-removebg-preview.png';
 //import profilepic from '../images/justinProfilePic.jpeg';
 import testimonialImage from '../images/testimonial.webp';
 import {
